78b309d62a7378a4c859845fb9f573aeabd33dcc
Luke <nycthbre@gmail.com>
2024-06-15 16:58:31 +0200

    Exercise 1.8 completion (unicafe)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,15 +12,27 @@ const Feedback = () => {
   )
 }
 
-const Stat = (props) => {
+const Stat = ({name, counter}) => {
   return (
-    <p>{props.name} {props.counter}</p>
+    <p>{name} {counter}</p>
   )
 }
 
-const Statistics = () => {
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const avg = (good - bad) / all
+  const pos = good / all
+
   return (
-    <h1>statistics</h1>
+    <div>
+      <h1>statistics</h1>
+      <Stat counter={good} name='good'/>
+      <Stat counter={neutral} name='neutral'/>
+      <Stat counter={bad} name='bad'/>
+      <Stat counter={all} name='all'/>
+      <Stat counter={avg} name='average'/>
+      <Stat counter={`${pos} %`} name='positive'/>
+    </div>
   )
 }
 
@@ -29,43 +41,20 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [pos, setPos] = useState(0)
 
   const handleGoodClick = () => {
     let counter = good + 1
-    sumAllFeedback()
     setGood(counter)
-    calcAverage()
   }
 
   const handleNeutralClick = () => {
     let counter = neutral + 1
-    sumAllFeedback()
     setNeutral(counter)
-    calcAverage()
   }
 
   const handleBadClick = () => {
     let counter = bad + 1
-    sumAllFeedback()
     setBad(counter)
-    calcAverage()
-  }
-
-  const sumAllFeedback = () => {
-    let sum = all + 1
-    setAll(sum)
-  }
-
-  const calcAverage = () => {
-    let countGood = good * 1
-    let countBad = bad * (-1)
-    let countAvg = (countGood + countBad) / all
-    let positivePercCounter = (good / all) * 100
-    setAvg(countAvg)
-    setPos(positivePercCounter)
   }
 
   return (
@@ -74,13 +63,7 @@ const App = () => {
       <Button name='good' handleClick={handleGoodClick}/>
       <Button name='neutral' handleClick={handleNeutralClick}/>
       <Button name='bad' handleClick={handleBadClick}/>
-      <Statistics />
-      <Stat counter={good} name='good'/>
-      <Stat counter={neutral} name='neutral'/>
-      <Stat counter={bad} name='bad'/>
-      <Stat counter={all} name='all'/>
-      <Stat counter={avg} name='average'/>
-      <Stat counter={`${pos} %`} name='positive'/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
