12ddb52f312f0e4f28c4e7db0742acd8d5be7c43
Luke <nycthbre@gmail.com>
2024-06-15 16:47:40 +0200

    Exercise 1.7 completion (unicafe)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -29,20 +29,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [pos, setPos] = useState(0)
 
   const handleGoodClick = () => {
     let counter = good + 1
+    sumAllFeedback()
     setGood(counter)
+    calcAverage()
   }
 
   const handleNeutralClick = () => {
     let counter = neutral + 1
+    sumAllFeedback()
     setNeutral(counter)
+    calcAverage()
   }
 
   const handleBadClick = () => {
     let counter = bad + 1
+    sumAllFeedback()
     setBad(counter)
+    calcAverage()
+  }
+
+  const sumAllFeedback = () => {
+    let sum = all + 1
+    setAll(sum)
+  }
+
+  const calcAverage = () => {
+    let countGood = good * 1
+    let countBad = bad * (-1)
+    let countAvg = (countGood + countBad) / all
+    let positivePercCounter = (good / all) * 100
+    setAvg(countAvg)
+    setPos(positivePercCounter)
   }
 
   return (
@@ -55,6 +78,9 @@ const App = () => {
       <Stat counter={good} name='good'/>
       <Stat counter={neutral} name='neutral'/>
       <Stat counter={bad} name='bad'/>
+      <Stat counter={all} name='all'/>
+      <Stat counter={avg} name='average'/>
+      <Stat counter={`${pos} %`} name='positive'/>
     </div>
   )
 }
